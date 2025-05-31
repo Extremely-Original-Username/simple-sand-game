83eae7cbd7d96246aa380b50709f680438ee9e84
Josh <joshinnes05@gmail.com>
2025-05-31 03:24:53 +0100

    added working shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <GL/glext.h>
 #include <string>
 #include <cstdlib>
 #include "mainHelpers.h"
+#include <GLES3/gl3.h>
+
 using namespace std;
 
 const string title = "Simple Sand!";
 const int windowWidth = 500;
 const int windowHeight = 450;
-const int gridWidth = 100;
-const int gridHeight = 90;
+const int gridWidth = 10;
+const int gridHeight = 9;
+
+const char *vertexShaderSource = "#version 460 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "}\0";
 
 int main() {
     cout << "Starting...\n";
@@ -30,16 +40,48 @@ int main() {
     cout << "Setting context...\n";
     glfwMakeContextCurrent(window);
 
+    //Must happen AFTER the context is set
+    cout << "Compiling shaders...\n";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);    
+    checkShaderCompileErrors(vertexShader);
+    
+
     cout << "Initialising data...\n";
     bool sandGrid [gridWidth][gridHeight];
-    for (int x = 0; x < gridWidth; x++)
+    for (int y = 0; y < gridHeight; y++)
     {
-        for (int y = 0; y < gridHeight; y++)
+        for (int x = 0; x < gridWidth; x++)
         {
             sandGrid[x][y] = rand() % 3 == 0;
         }
     }
-    
+
+    float drawPlaneVertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left 
+   };
+
+   unsigned int drawPlaneindices[] = {
+       0, 1, 3,   // first triangle
+       1, 2, 3    // second triangle
+   };  
+
+    //Create vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);  
+
+    //Bind buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    //Copy verted data to buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(drawPlaneVertices), drawPlaneVertices, GL_DYNAMIC_DRAW);
+
 
     cout << "Beginning main loop...\n";
     while (!glfwWindowShouldClose(window))
--- src/mainHelpers.h
+++ src/mainHelpers.h
@@ -2,6 +2,7 @@
 #define MAIN_HELPERS_H
 
 #include <GLFW/glfw3.h>
+#include <GLES3/gl3.h>
 
 // Callback to adjust the viewport when the window is resized
 inline void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -14,6 +15,19 @@ inline void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+// Function to check for shader compilation errors
+inline void checkShaderCompileErrors(unsigned int shader)
+{
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 }
 
 #endif // MAIN_HELPERS_H
