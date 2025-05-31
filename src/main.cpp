aae2a48500f926972235e3b9083271f63128dca9
Josh <joshinnes05@gmail.com>
2025-05-31 21:22:48 +0100

    got a square to render

--- src/main.cpp
+++ src/main.cpp
@@ -88,13 +88,25 @@ int main() {
         0.5f,  0.5f, 0.0f,  // top right
         0.5f, -0.5f, 0.0f,  // bottom right
        -0.5f, -0.5f, 0.0f,  // bottom left
-       //-0.5f,  0.5f, 0.0f   // top left 
+       -0.5f,  0.5f, 0.0f   // top left 
    };
 
-   unsigned int drawPlaneindices[] = {
+   unsigned int drawPlaneIndices[] = {
        0, 1, 3,   // first triangle
        1, 2, 3    // second triangle
    };  
+
+    /*
+    Helper for me:
+        A VAO is basically a preset of buffer bindings to a shader. So, when you want to use a certain shader 
+        with a certain group of buffers ( index, vertex, Texcoord, etc) you can setup a VAO with that, and every 
+        time.you switch to it, those bindings will be restored.
+
+        VBO is what I just called a buffer. Nothing more.
+
+        EBO is a VBO that is used to store vertex indices instead of actual data
+        https://www.reddit.com/r/opengl/comments/1eks98r/in_terms_of_vao_vbo_and_ebo_what_is_the_best_way/
+   */
 
    //Create Vertex Array Object to hold multiple VBOs if needed
    unsigned int VAO;
@@ -108,12 +120,19 @@ int main() {
     //Bind buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-    //Copy verted data to buffer
+    //Copy vertex data to buffer
     glBufferData(GL_ARRAY_BUFFER, sizeof(drawPlaneVertices), drawPlaneVertices, GL_DYNAMIC_DRAW);
 
     //Link vertex data to shader vertex attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);  
+
+    //Create and bind element buffer object to hold indices data
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(drawPlaneIndices), drawPlaneIndices, GL_DYNAMIC_DRAW); 
+
 
     cout << "Beginning main loop...\n";
     while (!glfwWindowShouldClose(window))
@@ -124,8 +143,10 @@ int main() {
         //Use shader program
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        //Bind and draw the EBO
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
 
         /* Swap front and back buffers */
