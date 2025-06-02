f1a7c7df264678b9037196536f93da06a189bb9b
Josh <joshinnes05@gmail.com>
2025-06-02 22:57:16 +0100

    semantic improvements to shader

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,6 +1,8 @@
 #version 460 core
-layout (location = 0) in vec3 aPos;
+
+layout (location = 0) in vec3 vectorPosition;
+
 void main()
 {
-   gl_Position = vec4(aPos, 1.0);
+   gl_Position = vec4(vectorPosition.x, vectorPosition.y, vectorPosition.z, 1.0);
 }
